7f92be8aedda5ae1826df74a589cd25ff4bae792
T.MAHARANI <maharani1118@gmail.com>
2017-12-19 15:54:12 +0530

    Create optimal.cpp

---
+++ Patterns/optimal.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n=0,num,i,j,val=0,temp=0;
+  cin>>n;
+   temp=(2*n)+1;
+   cout<<"temp="<<temp<<"\n";
+   for(i=1;i<=((2*n)-1);i++)
+    {
+        num=n+1;
+      if(i<=n)
+       {val++;temp--;}
+       else
+      {temp++;val--;}
+
+      for(j=1;j<=((2*n)-1);j++)
+      {
+        if(j<=val)
+         num--;
+        else if(j>=temp)
+         num++;
+         cout<<num;
+        }
+      cout<<"\n";
+    }
+
+}
