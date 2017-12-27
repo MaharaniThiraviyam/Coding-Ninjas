621f967bee11f19a822ce1df5c3905031399a8ee
T.MAHARANI <maharani1118@gmail.com>
2017-12-27 23:25:44 +0530

    Create gcd.cpp

---
+++ Number Theory/gcd.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, i, gcd;
+     cin>>n1>>n2;
+    for(i=1; i <= n1 && i <= n2; ++i)
+    {
+        // Checks if i is factor of both integers
+        if(n1%i==0 && n2%i==0)
+            gcd = i;
+    }
+
+    cout<<gcd;
+    return 0;
+}
