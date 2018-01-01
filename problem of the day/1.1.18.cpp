76703686a555d7b139bfb9bea9c49a5f72e81dd8
T.MAHARANI <maharani1118@gmail.com>
2018-01-01 19:26:21 +0530

    Add files via upload

---
+++ problem of the day/1.1.18.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ int main(){
+  int arr[10],i,n;
+  cin>>n;
+   for(i=0;i<n;i++)
+     cin>>arr[i];
+
+
+    for(i=0;i<l;i++)
+     if((arr[i]/4)>0)
+       arr[i]=arr[i]/4;
+      else
+       arr[i]=-1;
+
+
+   for(i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+    cout<<"\n count="<<count;
+
+ }
