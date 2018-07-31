5d579536c7086c32b47248cf6d53f0635b01fffa
T.MAHARANI <maharani1118@gmail.com>
2018-07-31 19:52:33 +0530

    Add files via upload

---
+++ problem of the day/31.7.18.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+   int n,m,row,col,i,j;
+   cin>>n>>m;
+    int arr[n][m];
+
+    for(i=0;i<n;i++){
+         for(j=0;j<m;j++)
+          cin>>arr[i][j];
+    }
+
+    for(j=0;j<m;j++){
+      if(j%2==0){
+          for(i=0;i<n;i++)
+           cout<<arr[i][j]<<" ";
+      }
+      else{
+           for(i=n-1;i>=0;i--)
+           cout<<arr[i][j]<<" ";
+
+      }
+
+    }
+
+
+    return 0;
+}
+
